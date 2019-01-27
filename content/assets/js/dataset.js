7b19c1e3784f02e6d60f40cb7f72459f655c93f8
Chip Hogg <charles.r.hogg@gmail.com>
2019-01-27 11:13:27 -0500

    Fix wonky clang-formatting
    Added a `.clang-format` file, and changed my executable from v4 to v5
    (see also: https://stackoverflow.com/q/41562308/1523582).

--- content/assets/js/dataset.js
+++ content/assets/js/dataset.js
@@ -7,21 +7,24 @@
 //    A function to get the next version of the dataset.
 
 // A linearly spaced range of numbers.
-function xRange(first, last, num_pts) {
-  return Array.from({length : num_pts},
-                    (_, i) => first + (last - first) * i / (num_pts - 1));
-  }
+function xRange(first, last, num_pts)
+{
+  return Array.from(
+      {length : num_pts}, (_, i) => first + (last - first) * i / (num_pts - 1));
+}
 
 // A dataset that does not change.
-function staticDataset(data) {
+function staticDataset(data)
+{
   return {
     data : Object.assign({}, data),
     advance : () => {},
   };
-  }
+}
 
 // An aggregate dataset made of other individual datasets.
-function combine(datasets) {
+function combine(datasets)
+{
   var combined_dataset =
       datasets.reduce((result, d) => Object.assign(result, d.data), {});
 
@@ -29,57 +32,63 @@ function combine(datasets) {
     data : combined_dataset,
     advance : () => datasets.forEach(d => d.advance()),
   };
-  }
+}
 
 // Add synthetic data to a dataset.
-function augment(dataset, augmentation) {
+function augment(dataset, augmentation)
+{
   // Make a shallow copy of the references: we want to stay up to date on
   // changes in the data, but we don't want to change the entries in the
   // "parent" dataset.
-  var data = Object.assign({}, dataset.data)
-  augmentation.update_derived_data(data)
+  var data = Object.assign({}, dataset.data);
+  augmentation.update_derived_data(data);
 
-      // Advance the underlying data, and recompute the augmented data.
-      function
-      advance() {
-        dataset.advance();
-        augmentation.update_derived_data(data)
-      }
+  // Advance the underlying data, and recompute the augmented data.
+  function advance()
+  {
+    dataset.advance();
+    augmentation.update_derived_data(data)
+  }
 
   return {
     data : data,
     advance : advance,
   };
-  }
+}
 
 // Augment dataset by applying a map to some existing data.
-function mapAugmentations(augmentations) {
+function mapAugmentations(augmentations)
+{
   // A utility to find the shortest data trace that's an input to the function.
   //
   // Could easily be removed from this class.
-  function _shortest_length(keys, dict) {
+  function _shortest_length(keys, dict)
+  {
     return Math.min(...keys.map(k => dict[k].length));
-    }
+  }
 
   // Apply the map to every affected element.
-  function _apply_augmentation(label, aug, data) {
+  function _apply_augmentation(label, aug, data)
+  {
     // Ensure the array exists.
-    if (!data[label]) {
+    if (!data[label])
+    {
       data[label] = [];
-      }
+    }
 
     // Update the data in the array.
-    for (var i = 0, i_stop = _shortest_length(aug.vars, data); i < i_stop;
-         ++i) {
+    for (var i = 0, i_stop = _shortest_length(aug.vars, data); i < i_stop; ++i)
+    {
       data[label][i] = aug.xform(aug.vars.map(k => data[k][i]))
     }
-    }
+  }
 
   // Apply every augmentation.
-  function update_derived_data(data) {
+  function update_derived_data(data)
+  {
     Object.entries(augmentations)
         .forEach(([ label, aug ]) => _apply_augmentation(label, aug, data));
-    }
+  }
 
   return {
     update_derived_data : update_derived_data,
